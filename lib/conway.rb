a82b4217b4e482e1f2124341af727857645046f3
Helen Thi <ht5658@gmail.com>
2015-01-20 15:36:43 -0500

    working on class Cell for conway test

---
+++ lib/conway.rb
@@ -0,0 +1,21 @@
+class Cell
+
+  def initialize(x, y)
+    @pos_x = x
+    @pos_y = y
+    @position_xy = [@pos_x, @pos_y]
+  end
+
+  def find_pos
+    @position_xy
+  end
+
+  def find_cell_type
+
+  end
+
+
+end
+
+#define grid class
+#number of cells
--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -1,1 +1,19 @@
 require_relative '../lib/conway'
+
+describe Cell do
+  # before(:all) do #before anything else---execute this code
+  #   #@cell = Cell.new(0, 1)
+  # end
+
+  describe '#find_pos' do #tests getter
+    it 'finds the position x and y of the cell' do
+      expect(@cell.find_pos).to eq [0, 1]
+    end
+  end
+
+  describe '#find_cell_type' do
+    it 'describes whether a cell is border, center, or corner cell' do
+      @cell = [0, 1]
+      expect(@cell.find_cell_type).to eq "border"
+    end
+  end
